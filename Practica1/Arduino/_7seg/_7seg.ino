0e0da56f5c9cf4ec4755344efa1b6478fed511d5
ANDREA MARIN ALARCON <andrea.marin@itam.mx>
2019-01-25 12:19:40 -0600

    Version final Practica1

--- Practica1/Arduino/_7seg/_7seg.ino
+++ Practica1/Arduino/_7seg/_7seg.ino
@@ -5,13 +5,14 @@ int d = 49;
 int e = 47;
 int f = 41;
 int g = 39;
+int num;
+String strNum;
 
 void setup(){
   pinMode(a, OUTPUT);
   pinMode(b, OUTPUT);
   pinMode(f, OUTPUT);
   pinMode(g, OUTPUT);
-  
   pinMode(c, OUTPUT);
   pinMode(d, OUTPUT);
   pinMode(e, OUTPUT);
@@ -19,12 +20,53 @@ void setup(){
 }
 
 void loop(){
-  digitalWrite(c, LOW);
-  digitalWrite(d, LOW);
-  digitalWrite(e, LOW);
-  digitalWrite(a, LOW);
-  digitalWrite(b, LOW);
-  digitalWrite(f, LOW);
-  digitalWrite(g, LOW);
+  strNum = Serial.readString();
+
+  if (strNum != NULL){
+      if(strNum == "1" || strNum == "4"){
+        digitalWrite(a, HIGH);
+      }else{
+        digitalWrite(a, LOW);
+      }
+    
+      if(strNum == "5" || strNum == "6" || strNum == "C" || strNum == "E" || strNum == "F" ){
+        digitalWrite(b, HIGH);
+      }else{
+        digitalWrite(b, LOW);
+      }
+    
+      if(strNum == "2" || strNum == "C" || strNum == "E" || strNum == "F"){
+        digitalWrite(c, HIGH);    
+      }else{
+        digitalWrite(c, LOW);
+      }
+    
+      if(strNum == "1" || strNum == "4" || strNum == "7" || strNum == "A" || strNum == "F"){
+        digitalWrite(d, HIGH);    
+      }else{
+        digitalWrite(d, LOW);
+      }
+    
+      if(strNum == "1" || strNum == "3" || strNum == "4" || strNum == "5" || strNum == "7" ||strNum == "9"){
+        digitalWrite(e, HIGH);     
+      }else{
+        digitalWrite(e, LOW);
+      }
+    
+      if(strNum == "1" || strNum == "2" || strNum == "3" || strNum == "7"){
+        digitalWrite(f, HIGH);
+      }else{
+        digitalWrite(f, LOW);
+      }
+    
+      if(strNum == "1" || strNum == "7" || strNum == "C" || strNum == "D" ){
+        digitalWrite(g, HIGH);
+      }else{
+        digitalWrite(g, LOW);
+      }
+  }
+
+  
   
 }
+
