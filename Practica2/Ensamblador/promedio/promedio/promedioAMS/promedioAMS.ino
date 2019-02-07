23b9d792f33e37f35155f8d365301123e1e365e8
andreamarin <andrea.marin2411@gmail.com>
2019-02-07 17:32:21 -0600

    promedio ams

--- Practica2/Ensamblador/promedio/promedio/promedioAMS/promedioAMS.ino
+++ Practica2/Ensamblador/promedio/promedio/promedioAMS/promedioAMS.ino
@@ -3,25 +3,30 @@ void setup() {
 }
 
 void loop() {
-  int n = (Serial.readString()).toInt();
-  int avg = 0;
+  uint8_t n = (Serial.readString()).toInt();
   uint8_t arr[5] = {2,5,7,3,4} ;
+  uint8_t *ptr = arr;
   /*
   for(int i = 0; i < n; i++){
     Serial.println("Escribe el numero "+i);
     arr[i] = (Serial.readString()).toInt();
   }
   */
-  //int *pointer = &arr;
 
-  
   asm volatile(
-    "ldi r16 0x00 \n \t"
+    "ldi r16, 0x00 \n \t"
+    "ldi r17 %1"
     "loop: \n \t"
-    "lds r20 x+ \n \t"
-    ""
-    :"=r" (avg)
-    :"d" (n), "x" (&arr)
+    "ldr r18, [%a2+] \n \t"
+    "add %0, r18 \n \t"
+    "inc r16"
+    "cpse r17, r16"
+    "jmp loop"
+    "break"
+    :"=&r" (sum)
+    :"d" (n-1), "e" (ptr)
     );
+
+  double avg = sum/n;
 
 }
